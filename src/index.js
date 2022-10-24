a9c54667dc01ae756933a0b970ad4a0cc0630bc0
EnVy <envy7321@gmail.com>
2022-10-24 11:52:46 +0300

    save

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './sass/main.scss';
+import './js/slider';
+import './js/contacts-modal';
+import './js/about-modal';
+import './js/mobile-menu';
+import './js/modal-form';
+import './js/scroll-up';
+import './js/buy-now';
+import './js/open-close';
